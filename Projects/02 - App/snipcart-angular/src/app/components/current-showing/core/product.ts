f5ca138c4ca09a39bdeb517660bdc2df69a5e6f9
IronPop34 <97186230+IronPop34@users.noreply.github.com>
2022-03-12 13:00:35 -0500

    price update

--- Projects/02 - App/snipcart-angular/src/app/components/current-showing/core/product.ts
+++ Projects/02 - App/snipcart-angular/src/app/components/current-showing/core/product.ts
@@ -4,6 +4,6 @@ export interface Product {
     id: number;
     name: string;
     imageUrls: string[];
-    price: number;
+    price: string[];
     sizes: Size[];
 }
--- Projects/02 - App/snipcart-angular/src/app/components/current-showing/current-showingAssets/currentExhibition.ts
+++ Projects/02 - App/snipcart-angular/src/app/components/current-showing/current-showingAssets/currentExhibition.ts
@@ -1,4 +1,5 @@
 import { Product } from '../core/product';
+import { Price } from '../core/price';
 import { Size } from '../core/size';
 
 export const PRODUCT: Product[] = [
@@ -6,69 +7,69 @@ export const PRODUCT: Product[] = [
     id: 1,
     name: 'The Carousel in Black & White #15',
     imageUrls: ['../current-showing/current-showingAssets/01 - The Carousel in Black & White #15.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 2,
     name: 'The Carousel in Black & White #20',
     imageUrls: ['../current-showing/current-showingAssets/02 - The Carousel in Black & White #20.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 3,
     name: 'The Carousel in Color #27',
     imageUrls: ['../current-showing/current-showingAssets/03 - The Carousel in Color #27.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 4,
     name: 'The Carousel in Color #26',
     imageUrls: ['../current-showing/current-showingAssets/04 - The Carousel in Color #26.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 5,
     name: 'The Carousel in Color #17',
     imageUrls: ['../current-showing/current-showingAssets/05 - The Carousel in Color #17.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 6,
     name: 'The Carousel in Color #21',
     imageUrls: ['../current-showing/current-showingAssets/06 - The Carousel in Color #21.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   }, 
   {
     id: 7,
     name: 'The Carousel in Color #14',
     imageUrls: ['../current-showing/current-showingAssets/07 - The Carousel in Color #14.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   }, {
     id: 8,
     name: 'The Carousel in Black & White #05',
     imageUrls: ['../current-showing/current-showingAssets/08 - The Carousel in Black & White #05.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 9,
     name: 'The Carousel in Black & White #06',
     imageUrls: ['../current-showing/current-showingAssets/09 - The Carousel in Black & White #06.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
   {
     id: 10,
     name: 'The Carousel in Black & White #11',
     imageUrls: ['../current-showing/current-showingAssets/10 - The Carousel in Black & White #11.jpg'],
-    price: 4500, 130, 160, 240,
+    price: [Price.ORIGINAL, Price.SMALL, Price.MEDIUM, Price.LARGE],
     sizes: [Size.ORIGINAL, Size.SMALL, Size.MEDIUM, Size.LARGE],
   },
 ];
