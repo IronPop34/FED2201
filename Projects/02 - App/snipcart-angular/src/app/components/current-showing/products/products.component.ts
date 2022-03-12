a38fa040cb8819e13ccf0747c5dec9ec35b2293a
IronPop34 <97186230+IronPop34@users.noreply.github.com>
2022-03-12 13:10:10 -0500

    update

--- Projects/02 - App/snipcart-angular/src/app/components/current-showing/products/products.component.ts
+++ Projects/02 - App/snipcart-angular/src/app/components/current-showing/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PRODUCT } from '../current-showingAssets/currentExhibition';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  products = PRODUCT;
 
 }
